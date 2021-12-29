4cf82559599f218f40aa90641a4507325592316d
Antony Levitsky <antonylevitsky@gmail.com>
2021-12-29 17:25:19 +0300

    frontend ready

---
+++ src/script.js
@@ -0,0 +1,9 @@
+const app = new Application({
+  preparation: PreparationScene,
+  computer: ComputerScene
+});
+
+app.start("preparation");
+
+// document.querySelector('[data-action="randomize"').click();
+// document.querySelector('[data-computer="middle"').click();
